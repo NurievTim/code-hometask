6d1fef3e7d6f84ee6fcc5903ef1491071d9072f0
Tim <timurnuriev@bk.ru>
2022-09-03 22:54:11 +0300

    added ex_10,13,15

--- ex_13/Program.cs
+++ ex_13/Program.cs
@@ -3,3 +3,23 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+// int thirdDigit
+// int threeDigitNumber
+
+Console.Clear();
+Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("третьей цыфры нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    int thirdDigit = num % 10;
+    Console.WriteLine(thirdDigit);
+}
