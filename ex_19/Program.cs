639f3b29cbf28bc51009912e68f5ae4f70bdecf2
Tim <timurnuriev@bk.ru>
2022-09-08 23:29:53 +0300

    added and done ex_21

--- ex_19/Program.cs
+++ ex_19/Program.cs
@@ -1,29 +1,4 @@
-﻿// Задача 20: Напишите программу, которая
-// принимает на вход координаты двух точек и
-// находит расстояние между ними в 2D
-// пространстве.
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
-
-Console.Clear();
-Console.Write("Пользователь, введите координаты первой точки: ");
-Console.Write("Х1: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Y1: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Пользователь, введите координаты второй точки: ");
-Console.Write("X2: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Y2: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-double ABDistance(int xc1, int yc1, int xc2, int yc2)
-{
-    double d = (xc2 - xc1) * (xc2 - xc1) + (yc2 - yc1) * (yc2 - yc1);
-    return Math.Round(Math.Sqrt(d), 2, MidpointRounding.ToZero);
-}
-
-
-Console.WriteLine(ABDistance(x1, y1, x2, y2));
-
-
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
---
+++ ex_21/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7) -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты первой точки: ");
+
+Console.Write("Х1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Пользователь, введите координаты второй точки: ");
+
+Console.Write("X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int xc1, int yc1, int zc1, int xc2, int yc2, int zc2)
+{
+    double dist = (xc2 - xc1) * (xc2 - xc1) + (yc2 - yc1) * (yc2 - yc1) + (zc2 - zc1) * (zc2 - zc1);
+    return Math.Round(Math.Sqrt(dist), 2, MidpointRounding.ToEven);
+}
+Console.WriteLine();
+Console.WriteLine($"Расстояния между точками - {(Distance(x1, y1, z1, x2, y2, z2))}");
